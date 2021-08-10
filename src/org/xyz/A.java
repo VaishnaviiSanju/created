d89f0988a170cb2f844447d65e6647af925b582f
VaishnaviiSanju <vaishoff@gmail.com>
2021-08-10 19:06:58 +0530

    A modified

--- src/org/xyz/A.java
+++ src/org/xyz/A.java
@@ -5,8 +5,12 @@ private void Sysout() {
 	System.out.println("a");
 
 }
+private void add() {
+	System.out.println("b");
+}
 public static void main(String[] args) {
 	A a = new A();
 	a.Sysout();
+	a.add();
 }
 }
